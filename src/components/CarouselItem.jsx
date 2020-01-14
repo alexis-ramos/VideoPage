424abc991cfa1497e69066c700b867192d388fad
Alexis Ramos <alexisramoscaballero@gmail.com>
2020-01-14 01:32:11 -0600

    Se agregan los proptypes para validar los tipos de datos que se envian por prop

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import "../assets/styles/components/CarouselItem.scss";
 import playIcon from "../assets/static/play-icon.png";
 import plusIcon from "../assets/static/plus-icon.png";
@@ -26,5 +27,15 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     </div>
   </div>
 );
+
+// proptypes sirve para poder validar los tipos de datos que mandamos
+
+CarouselItem.prototype = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+}
 
 export default CarouselItem;
