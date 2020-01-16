0368ec508cd0e62867520339c4bfffc0ba73d22d
Alexis Ramos <alexisramoscaballero@gmail.com>
2020-01-15 20:56:25 -0600

    Se agrega Link para manejar la navegacion en la app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../assets/styles/components/Header.scss";
 import logo from "../assets/static/logo-platzi-video-colors.png";
 import userIcon from "../assets/static/user-icon.png";
 
 const Header = () => (
-  <header className="header">
-    <img className="header__img" src={logo} alt="Platzi Video" />
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="userIcon" />
+  <header className='header'>
+    <Link to='/'>
+      <img className='header__img' src={logo} alt='Platzi Video' />
+    </Link>
+    <div className='header__menu'>
+      <div className='header__menu--profile'>
+        <img src={userIcon} alt='userIcon' />
         <p>Perfil</p>
       </div>
       <ul>
         <li>
-          <a href="/">Cuenta</a>
+          <Link to='/'>Cuenta</Link>
         </li>
         <li>
-          <a href="/">Cerrar Sesión</a>
+          <Link to='/login'>Iniciar Sesión</Link>
         </li>
       </ul>
     </div>
